cd6aba1b3dcbdf8211786497d5743deab7e30319
Vaidic Dodwani <vaidicdodwani@gmail.com>
2025-04-06 18:20:13 +0530

    feat: refactor HandGestureViewer to use StatefulWidget and manage animation state

--- lib/view/shared/hand_gesture_enum.dart
+++ lib/view/shared/hand_gesture_enum.dart
@@ -27,4 +27,23 @@ extension HandGestureExtension on HandGestures {
         return 'Six';
     }
   }
+
+  int get value {
+    switch (this) {
+      case HandGestures.none:
+        return 0;
+      case HandGestures.one:
+        return 1;
+      case HandGestures.two:
+        return 2;
+      case HandGestures.three:
+        return 3;
+      case HandGestures.four:
+        return 4;
+      case HandGestures.five:
+        return 5;
+      case HandGestures.six:
+        return 6;
+    }
+  }
 }
--- lib/view/shared/hand_gesture_viewer.dart
+++ lib/view/shared/hand_gesture_viewer.dart
@@ -6,7 +6,7 @@ import 'package:rive/rive.dart' as rive;
 
 import '../../utils/constants/app_constants.dart';
 
-class HandGestureViewer extends StatelessWidget {
+class HandGestureViewer extends StatefulWidget {
   final HandGestures gesture;
   final int angle;
   final double scaleX;
@@ -18,17 +18,38 @@ class HandGestureViewer extends StatelessWidget {
     this.scaleX = 1,
   });
 
+  @override
+  State<HandGestureViewer> createState() => _HandGestureViewerState();
+}
+
+class _HandGestureViewerState extends State<HandGestureViewer> {
+  rive.Artboard? _artboard;
+  rive.SimpleAnimation? _controller;
+
+  @override
+  void didUpdateWidget(covariant HandGestureViewer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.gesture != widget.gesture && _artboard != null) {
+      _artboard!.removeController(_controller!);
+      _controller = rive.SimpleAnimation(widget.gesture.name);
+      _artboard!.addController(_controller!);
+      setState(() {});
+    }
+  }
+
   void _onRiveInit(rive.Artboard artboard) {
-    final controller = rive.SimpleAnimation(gesture.name);
-    artboard.addController(controller);
+    _artboard = artboard;
+    _controller = rive.SimpleAnimation(widget.gesture.name);
+    artboard.addController(_controller!);
   }
 
   @override
   Widget build(BuildContext context) {
     return Transform.scale(
-      scaleX: scaleX,
+      scaleX: widget.scaleX,
       child: Transform.rotate(
-        angle: angle * (pi / 180),
+        angle: widget.angle * (pi / 180),
         child: rive.RiveAnimation.asset(
           AppAssets.handGestureRive,
           fit: BoxFit.contain,
